5f8f38a90698559260cb94300068cd618d3c2b75
Gianpiero Condorelli <gia.condorelli@gmail.com>
2019-02-15 16:31:01 +0100

    other changes

---
+++ src/app/shared/cloud-words/cloud-words.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cloud-words',
+  templateUrl: './cloud-words.component.html',
+  styleUrls: ['./cloud-words.component.scss']
+})
+export class CloudWordsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
