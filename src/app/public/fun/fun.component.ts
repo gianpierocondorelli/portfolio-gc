596cdb309dadedf9c0e1e4ef3c2a714019917616
Gianpiero Condorelli <g.condorelli@reply.it>
2019-02-16 11:30:16 +0100

    fixed parallax

--- src/app/public/fun/fun.component.ts
+++ src/app/public/fun/fun.component.ts
@@ -9,70 +9,99 @@ import { BaseComponent } from 'src/app/shared/base-component';
 export class FunComponent extends BaseComponent implements OnInit {
 
   lang = 'en';
-  disableMaps = false;
+  private activateVisibility = false;
 
-  marker = [{
-    city: 'Rome',
-    country: 'Italy',
-    latitude: 41.9109,
-    longitude: 12.4818
-  }];
-
-  markers = [{
-    city: 'Rome',
-    country: 'Italy',
-    latitude: 41.9109,
-    longitude: 12.4818
+  countries = [{
+    markers: [{
+      city: 'Rome',
+      country: 'Italy',
+      latitude: 41.9109,
+      longitude: 12.4818
+    },
+    {
+      city: 'Amsterdam',
+      country: 'Nederlands',
+      latitude: 52.37403,
+      longitude: 4.88969
+    },
+    {
+      city: 'London',
+      country: 'United Kingdom',
+      latitude: 51.50853,
+      longitude: -0.12574
+    },
+    {
+      city: 'Paris',
+      country: 'France',
+      latitude: 48.85341,
+      longitude: 2.3488
+    },
+    {
+      city: 'Barcelona',
+      country: 'Spain',
+      latitude: 41.38879,
+      longitude: 2.15899
+    },
+    {
+      city: 'Zurich',
+      country: 'Switzerland',
+      latitude: 47.3666700,
+      longitude: 8.5500000
+    },
+    {
+      city: 'Munich',
+      country: 'Germany',
+      latitude: 48.1374300,
+      longitude: 11.5754900
+    }
+    ]
   },
   {
-    city: 'Amsterdam',
-    country: 'Nederlands',
-    latitude: 52.37403,
-    longitude: 4.88969
+    name: 'Italy',
+    markers: [{
+      city: 'Rome',
+      country: 'Italy',
+      latitude: 41.9109,
+      longitude: 12.4818
+    }]
   },
   {
-    city: 'London',
-    country: 'United Kingdom',
-    latitude: 51.50853,
-    longitude: -0.12574
-  },
-  {
-    city: 'Paris',
-    country: 'France',
-    latitude: 48.85341,
-    longitude: 2.3488
-  },
-  {
-    city: 'Barcelona',
-    country: 'Spain',
-    latitude: 41.38879,
-    longitude: 2.15899
-  },
-  {
-    city: 'Zurich',
-    country: 'Switzerland',
-    latitude: 47.3666700,
-    longitude: 8.5500000
-  },
-  {
-    city: 'Munich',
-    country: 'Germany',
-    latitude: 48.1374300,
-    longitude: 11.5754900
+    name: 'France',
+    markers: [{
+      city: 'Paris',
+      country: 'France',
+      latitude: 48.85341,
+      longitude: 2.3488
+    }]
   }
   ];
+
 
   constructor(injector: Injector) {
     super(injector);
   }
 
   ngOnInit() {
+    setTimeout(() => {
+      this.activateVisibility = true;
+    }, 100);
   }
 
-
-  @HostListener('window:scroll', ['$event']) // for window scroll events
-  onScroll(event: Event) {
-    console.log('event', event);
-    this.disableMaps = window.scrollY > 150;
+  getVisibility(index: number) {
+    if (this.activateVisibility) {
+      const introductionHeight = document.getElementsByClassName('introduction')[0].clientHeight;
+      const subtractInit = (introductionHeight / 4);
+      const otherContainerHeight = document.getElementsByClassName('other-container')[0].clientHeight;
+      const subtractOther = (otherContainerHeight / 4);
+      const currentScroll = window.scrollY;
+      if (currentScroll <= introductionHeight - subtractInit) {
+        return index === 0;
+      } else {
+        return currentScroll > introductionHeight + (otherContainerHeight * (index - 1)) - subtractOther &&
+          currentScroll < introductionHeight + (otherContainerHeight * index) - subtractOther;
+      }
+    }
+    return false;
   }
+
 }
