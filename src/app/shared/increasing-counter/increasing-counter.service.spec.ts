0e9340d204595729bcfed8f135fe6bf41e0bc944
Gianpiero Condorelli <gia.condorelli@gmail.com>
2019-02-25 17:58:24 +0100

    new updates

---
+++ src/app/shared/increasing-counter/increasing-counter.service.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+
+import { IncreasingCounterService } from './increasing-counter.service';
+
+describe('IncreasingCounterService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: IncreasingCounterService = TestBed.get(IncreasingCounterService);
+    expect(service).toBeTruthy();
+  });
+});
