6b0c69fbceed7466a247502384383d9e5830c2d4
Gianpiero Condorelli <gia.condorelli@gmail.com>
2020-06-03 20:39:37 +0100

    Removing logging

--- src/main.browser.ts
+++ src/main.browser.ts
@@ -6,11 +6,5 @@ import { AppBrowserModule } from './app/app.browser.module'
 if (environment.production) {
   enableProdMode()
 }
-// for showing loading indicator
-platformBrowserDynamic()
-  .bootstrapModule(AppBrowserModule)
-  .then(() => {
-    console.log('LOADED')
-    // hide loading here dom.remove()
-  })
-  .catch((err) => console.log(err))
+
+platformBrowserDynamic().bootstrapModule(AppBrowserModule)
--- src/main.server.ts
+++ src/main.server.ts
@@ -1,3 +1,10 @@
+import { enableProdMode } from '@angular/core'
+import { environment } from 'environments/environment'
+
 export { AppServerModule } from './app/app.server.module'
 
 export { renderModule, renderModuleFactory } from '@angular/platform-server'
+
+if (environment.production) {
+  enableProdMode()
+}
