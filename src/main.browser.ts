21383483b3badcf2f44336175ae2e55d8299c7b4
Gianpiero Condorelli <gia.condorelli@gmail.com>
2020-06-09 23:33:47 +0100

    Fix environment pointing

--- src/main.browser.ts
+++ src/main.browser.ts
@@ -1,5 +1,5 @@
 import { enableProdMode } from '@angular/core'
-import { environment } from './environments/browser/environment'
+import { environment } from './environments/environment'
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
 import { AppBrowserModule } from './app/app.browser.module'
 
