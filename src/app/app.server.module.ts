e3ff8d55e541f5510c8e5ea4c3c9703b92d4117e
Gianpiero Condorelli <gia.condorelli@gmail.com>
2020-06-14 21:37:02 +0100

    Enable universal i18n

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -6,10 +6,24 @@ import {
 import { BrowserModule } from '@angular/platform-browser'
 import { NoopAnimationsModule } from '@angular/platform-browser/animations'
 import { HTTP_INTERCEPTORS } from '@angular/common/http'
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core'
 
 import { UniversalInterceptor } from './shared/http-interceptors/universal.interceptor'
 import { AppComponent } from './app.component'
 import { AppModule } from './app.module'
+import { Observable, Observer } from 'rxjs' 
+import { readFileSync } from 'fs'
+
+export function universalLoader(): TranslateLoader {
+  return {
+      getTranslation: (lang: string) => {
+          return Observable.create((observer: Observer<unknown>) => {
+              observer.next(JSON.parse(readFileSync(`./dist/browser/assets/i18n/${lang}.json`, 'utf8')));
+              observer.complete();
+          });
+      }
+  } as TranslateLoader;
+}
 
 @NgModule({
   bootstrap: [AppComponent],
@@ -27,6 +41,9 @@ import { AppModule } from './app.module'
     ServerModule,
     NoopAnimationsModule,
     ServerTransferStateModule, // comment
+    TranslateModule.forRoot({
+      loader: {provide: TranslateLoader, useFactory: universalLoader}
+  })
   ],
 })
 export class AppServerModule {}
